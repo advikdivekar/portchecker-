ee6b2f8b14c25547c33a6efd747dfb5f465b52ec
Vexx <advikdivekar@gmail.com>
2026-02-21 13:05:27 +0530

    Reduce socket timeout from 2 seconds to 1 second.

--- scanner.py
+++ scanner.py
@@ -4,7 +4,7 @@ import time
 def scan_port(target, port):
     try: 
         s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(2) #wait max 1 seconds 
+        s.settimeout(1) #wait max 1 seconds 
         result = s.connect_ex((target, port))
         s.close()
         return result == 0
