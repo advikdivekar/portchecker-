0ef6998d5f75c436e83b945ccb91af54a0189cb5
Vexx <advikdivekar@gmail.com>
2026-02-21 12:46:45 +0530

    checkp1

--- scanner.py
+++ scanner.py
@@ -1,1 +1,13 @@
-print("port scanner intialized")
+import socket 
+import time 
+
+def scan_port(target, port):
+    try: 
+        s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2) #wait max 1 seconds 
+        result = s.connect_ex((target, port))
+        s.close()
+        return result == 0
+    except: 
+        return False
+
